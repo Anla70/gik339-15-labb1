3b042d34949c59f08ad6ec06a85fbb6945992d75
Anla70 <98649933+Anla70@users.noreply.github.com>
2023-12-06 09:49:18 +0100

    onclick borttaget

--- js/script copy.js
+++ js/script copy.js
@@ -12,10 +12,11 @@ const divElement = document.getElementById("empty_box");
 
 // Här skapas en fördefinierad funktion
 function handleInputEvent(e) {
+  // Eventobjektet används för att få information om target och vi skriver ut infon till konsolen
   const target = e.target;
   console.log(`Input field ${target.name} triggered the event.`);
 
-  // Om avsändaren (target) är inputfältet content så ska div-elementet uppdateras
+  // Om avsändaren/target är inputfältet content så ska div-elementet uppdateras
   if (target.name === "content") {
     divElement.innerHTML = target.value;
   }
@@ -25,18 +26,23 @@ function handleInputEvent(e) {
 
 // Eventlyssnare till checkboxen
 checkbox.addEventListener("change", function () {
+  // Hämtar färgen från textfältet
   const colorInput = document.getElementById("color");
   const color = colorInput.value;
+
+  // Uppdaterar div-elementets bakgrundsfärg
   divElement.style.backgroundColor = color;
 });
 
 // Eventlyssnare till textfälten
 textfields.forEach(function (textfield) {
+  // Kopplar eventlyssnare till input och blur för våra textfält
   textfield.addEventListener("input", handleInputEvent);
   textfield.addEventListener("blur", handleInputEvent);
 });
 
 // Eventlyssnare till knappen
 button.addEventListener("click", function () {
+  // Tar bort div-elementet
   divElement.remove();
 });
